77204e5332c988cccf50a5b99d95c4a8fd99d3b1
Ravil Khabibullin <ravil.khabibullin94@gmail.com>
2021-03-02 14:21:14 +0300

    on_startup and on_shutdown

--- bot.py
+++ bot.py
@@ -1,6 +1,18 @@
-from handlers import dp
 from aiogram import executor
+
+from handlers import dp
+from utils.misc.loggers import log
+from utils.notify_admins import shutdown_notify, start_notify
+
+
+async def on_startup(dp):
+    await start_notify(dp)
+
+
+async def on_shutdown(dp):
+    await shutdown_notify(dp)
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=False)
+    log.warning("Starting the bot")
+    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
