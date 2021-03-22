76f68a1dfe1bbb47e1142039f646133b83484861
Ravil Khabibullin <ravil.khabibullin94@gmail.com>
2021-03-22 12:00:08 +0300

    update:

--- utils/misc/loggers.py
+++ utils/misc/loggers.py
@@ -4,5 +4,3 @@ import logging
 logging.basicConfig(filename="bot_logger.log",
                     format="%(asctime)s: %(name)s: %(levelname)s: %(funcName)s: [LINE: %(lineno)d]: %(message)s",
                     level=logging.INFO)
-
-log = logging.getLogger("bot_logger")
