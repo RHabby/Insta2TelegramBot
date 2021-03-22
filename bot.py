2477cfc63da1d1daa146e728b871b0ae11437a72
Ravil Khabibullin <ravil.khabibullin94@gmail.com>
2021-03-22 11:53:09 +0300

    basic handlers

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
+import logging
+
 from aiogram import executor
 
 from handlers import dp
-from utils.misc.loggers import log
+
 from utils.notify_admins import shutdown_notify, start_notify
 from utils.set_bot_commands import set_basic_commands
 
@@ -12,9 +14,14 @@ async def on_startup(dp):
 
 
 async def on_shutdown(dp):
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
     await shutdown_notify(dp)
 
 
 if __name__ == "__main__":
-    log.warning("Starting the bot")
-    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
+    logging.warning("Starting the bot")
+    executor.start_polling(dp, on_startup=on_startup,
+                           on_shutdown=on_shutdown,
+                           skip_updates=True)
--- handlers/users/common.py
+++ handlers/users/common.py
@@ -1,26 +1,24 @@
-from utils.misc.loggers import log
+import logging
 
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandHelp, CommandStart
+
 from loader import dp
 
-
-@dp.message_handler(CommandStart())
-async def start(message: types.Message):
-    log.info(
-        f"{message.chat.id} | {message.chat.first_name} {message.chat.last_name} | {message.chat.username} | {message.text}")
-    await message.answer("Hello, you started me.")
+from settings.text_templates import hello_msg, help_text
 
 
-@dp.message_handler(CommandHelp())
-async def help(message: types.Message):
-    text = [
-        "List of Commands:",
-        "/start — Start the bot;",
-        "/help — This help message;",
-        "",
-        "If you want to work with me, you have to follow for some rules:",
-        "1. Make sure that the link you send me belongs to an open profile;",
-        "2. may be something else..."
-    ]
-    await message.answer("\n".join(text))
+@dp.message_handler(CommandStart(), state="*")
+@dp.throttled(rate=2)
+async def start_cmd(message: types.Message):
+    logging.info(f'{message.chat.id} | {message.chat.full_name} | '
+                 f'{message.chat.username} | {message.text}')
+    await message.answer(text=hello_msg.format(user_name=message.chat.full_name))
+
+
+@dp.message_handler(CommandHelp(), state="*")
+@dp.throttled(rate=2)
+async def help_cmd(message: types.Message):
+    logging.info(f'{message.chat.id} | {message.chat.full_name} | '
+                 f'{message.chat.username} | {message.text}')
+    await message.answer(text=help_text)
