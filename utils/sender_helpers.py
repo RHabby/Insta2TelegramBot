70ff921debf2e76aa7f83c39740d0a2257bf9a1f
Ravil Khabibullin <ravil.khabibullin94@gmail.com>
2021-03-26 13:13:39 +0300

    refactor send_by_content_len func and media group [GIF files, Any] exception handling

--- utils/sender_helpers.py
+++ utils/sender_helpers.py
@@ -2,11 +2,11 @@ import unicodedata
 from typing import Dict, List, Tuple
 
 from aiogram import types
+import aiogram
 
 import demoji
 
 import iuliia
-
 
 from settings import config
 from settings.text_templates import error_post_caption
@@ -15,39 +15,61 @@ from settings.text_templates import error_post_caption
 async def send_by_content_len(
         msg: types.Message, post: Dict,
         caption: str, markup: types.InlineKeyboardMarkup) -> Tuple:
+
     if len(post["post_content"]) == 1:
-        try:
-            if ".mp4" in post["post_content"][0]:
-                sent = await msg.answer_video(video=rf'{post["post_content"][0]}',
-                                              caption=caption,
-                                              reply_markup=markup)
-            else:
-                sent = await msg.answer_photo(photo=rf'{post["post_content"][0]}',
-                                              caption=caption,
-                                              reply_markup=markup)
-        except Exception:
-            sent = await msg.answer(
-                text=error_post_caption.format(
-                    post_content=post["post_content"][0],
-                    caption=caption,
-                ),
-                disable_web_page_preview=False,
-                reply_markup=markup,
-            )
-        return sent, None
+        return await configure_simple_post(post=post["post_content"],
+                                           msg=msg, caption=caption,
+                                           markup=markup)
     elif len(post["post_content"]) > 1:
-        media = types.MediaGroup()
+        try:
+            return await configure_media_group(post=post["post_content"],
+                                               msg=msg, caption=caption,
+                                               markup=markup)
+        except aiogram.utils.exceptions.WrongFileIdentifier:
+            return [await configure_simple_post(post=[post], msg=msg,
+                                                caption=caption, markup=markup)
+                    for post in post["post_content"]][0]
 
-        for item in post["post_content"]:
-            if ".mp4" in item:
-                media.attach_video(video=rf"{item}")
-            else:
-                media.attach_photo(photo=rf"{item}")
 
-        sent = await msg.answer_media_group(media=media)
-        text_msg = await msg.answer(text=caption,
-                                    reply_markup=markup)
-        return sent, text_msg
+async def configure_simple_post(post: List, **kwargs):
+    msg = kwargs.get("msg")
+    caption = kwargs.get("caption")
+    markup = kwargs.get("markup")
+
+    try:
+        if ".mp4" in post[0]:
+            sent = await msg.answer_video(video=rf'{post[0]}',
+                                          caption=caption,
+                                          reply_markup=markup)
+        else:
+            sent = await msg.answer_photo(photo=rf'{post[0]}',
+                                          caption=caption,
+                                          reply_markup=markup)
+    except Exception:
+        sent = await msg.answer(
+            text=error_post_caption.format(
+                post_content=post[0],
+                caption=caption,
+            ),
+            disable_web_page_preview=False,
+            reply_markup=markup,
+        )
+    return sent, None
+
+
+async def configure_media_group(post: List, **kwargs):
+    msg = kwargs.get("msg")
+    media = types.MediaGroup()
+    for item in post:
+        if ".mp4" in item:
+            media.attach_video(video=rf"{item}")
+        else:
+            media.attach_photo(photo=rf"{item}")
+
+    sent = await msg.answer_media_group(media=media)
+    text_msg = await msg.answer(text=kwargs.get("caption"),
+                                reply_markup=kwargs.get("markup"))
+    return sent, text_msg
 
 
 def regroup_list(data: List, max_inner_list_len: int = 3):
