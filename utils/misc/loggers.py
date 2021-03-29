0f934e54ab7d9179feae33ced6685621ac47b4bf
Ravil Khabibullin <ravil.khabibullin94@gmail.com>
2021-03-29 10:31:10 +0300

    fix: filename path

--- utils/misc/loggers.py
+++ utils/misc/loggers.py
@@ -1,6 +1,6 @@
 import logging
 
 
-logging.basicConfig(filename="bot_logger.log",
+logging.basicConfig(filename="./bot_logger.log",
                     format="%(asctime)s: %(name)s: %(levelname)s: %(funcName)s: [LINE: %(lineno)d]: %(message)s",
                     level=logging.INFO)
