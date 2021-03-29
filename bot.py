41443284bf802da5f6247eb3fc31d426e94c5d58
Ravil Khabibullin <ravil.khabibullin94@gmail.com>
2021-03-29 11:59:53 +0300

    update:

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
-import logging
-
 from aiogram import executor
 
 from handlers import dp
 
-from utils.notify_admins import shutdown_notify, start_notify
+from utils.notify_admins import start_notify
 from utils.set_bot_commands import set_basic_commands
 
 
@@ -17,11 +15,8 @@ async def on_shutdown(dp):
     await dp.storage.close()
     await dp.storage.wait_closed()
 
-    await shutdown_notify(dp)
-
 
 if __name__ == "__main__":
-    logging.warning("Starting the bot")
     executor.start_polling(dp, on_startup=on_startup,
                            on_shutdown=on_shutdown,
                            skip_updates=True)
